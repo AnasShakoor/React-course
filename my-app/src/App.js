b37216b7d31de682515a21f2d931ee242da2ca67
AnasShakoor <147330270+AnasShakoor@users.noreply.github.com>
2024-06-24 03:40:11 +0500

    react course

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          and this is john From Fairwys Logistics 
+          after all I am the father of you and now we are not looking for it and also and now e are and now we are not looking for it and now we anoer
+          Learn React with me and you and him also you are stupid and you are not looking very good in it and also we do not need some thing we are looking for ok you 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
