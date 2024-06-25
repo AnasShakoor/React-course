806e0cf3e45c31df96d78bbba026724b44b91c1a
AnasShakoor <147330270+AnasShakoor@users.noreply.github.com>
2024-06-25 14:17:50 +0500

    video 6

---
+++ my-app/module1.mjs
@@ -0,0 +1,6 @@
+import abc,{b,a} from './module2.mjs';
+
+console.log(abc);
+console.log(a);
+console.log(b);
+
---
+++ my-app/module2.mjs
@@ -0,0 +1,8 @@
+let a = 'anas shakoor';
+let b = 'akram and ali';
+let c = 'going to school';
+
+
+export default c;   
+export  {b};
+export {a};
